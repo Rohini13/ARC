db7379205614df3bb3b843b4567b0a61a2564c71
Rohini13 <iit2018087@iiita.ac.in>
2019-02-09 06:18:54 +0530

    flask

--- demo.py
+++ demo.py
@@ -1,6 +1,41 @@
-import requests
-import pprint
+from flask import Flask, jsonify
+from flask import abort, request
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+app = Flask(__name__)
 
-x = requests.get("http://opencode-leaderboard.herokuapp.com/users/leaderboard")
+@app.route("/",methods=["GET"])
+def hello():
+  return "hello"
 
-pprint.pprint(x.text)
+@app.route("/", methods=["POST"])
+def sentiment_scores():
+          sentence=request.json
+          x = sentence["text"]
+          # return "Hello"
+        # Create a SentimentIntensityAnalyzer object. 
+          sid_obj = SentimentIntensityAnalyzer() 
+     
+        # polarity_scores method of SentimentIntensityAnalyzer 
+        # oject gives a sentiment dictionary. 
+        # which contains pos, neg, neu, and compound scores. 
+          sentiment_dict = sid_obj.polarity_scores(x) 
+
+          print("Overall sentiment dictionary is : ", sentiment_dict) 
+          print("sentence was rated as ", sentiment_dict['neg']*100, "% Negative") 
+          print("sentence was rated as ", sentiment_dict['neu']*100, "% Neutral") 
+          print("sentence was rated as ", sentiment_dict['pos']*100, "% Positive") 
+    
+          print("Sentence Overall Rated As") 
+    
+        # decide sentiment as positive, negative and neutral 
+          if sentiment_dict['compound'] >= 0.05 : 
+            return("Positive") 
+    
+          elif sentiment_dict['compound'] <= - 0.05 : 
+            return("Negative") 
+    
+          else : 
+            return("Neutral")
+
+if __name__ == '__main__':
+    app.run(port=3000)
